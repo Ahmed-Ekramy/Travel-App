0381e50b4cd404f4364479131f742a5016af4eec
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2024-01-21 01:13:14 -0900

    card item

---
+++ lib/core/theming/text_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle readexPro24w700() => GoogleFonts.readexPro(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    );
---
+++ lib/features/home/presentation/widgets/card_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CardItem extends StatelessWidget {
+
+  const CardItem(this.image,this.text, {super.key});
+
+  final String image;
+  final  String text;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shadowColor: Colors.grey,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        width: 146.w,
+        height: 200.h,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8)
+        ),
+        child: Column(
+            children: [
+              SvgPicture.asset(image),
+              SizedBox(height: 11.h,),
+              Text(
+                text,
+                textAlign: TextAlign.center,
+                style: readexPro24w700(),
+              )
+            ]),
+      ),
+    );
+  }
+}
