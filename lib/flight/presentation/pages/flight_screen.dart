14e41834a024247450b51263248ce5e64a993300
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2024-01-24 02:29:35 -0900

    tab bar and screen tab bar

---
+++ lib/flight/presentation/pages/flight_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/core/theming/text_style.dart';
+
+import '../tabs/go_back_tab.dart';
+import '../tabs/go_tab.dart';
+import '../tabs/multiple_trip_tabs.dart';
+
+class FlightScreen extends StatelessWidget {
+  const FlightScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+              physics: const BouncingScrollPhysics(),
+              indicatorColor: Colors.blue,
+              labelColor: Colors.blue,
+              labelStyle: readexPro16w700(),
+              unselectedLabelStyle:
+                  readexPro12w400().copyWith(color: Colors.grey),
+              tabs: const [
+                Tab(
+                    child: Text(
+                  "رحلات متعدده ",
+                )),
+                Tab(child: Text("ذهاب و عوده")),
+                Tab(child: Text("ذهاب ")),
+              ]),
+        ),
+        body: const TabBarView(
+            children: [
+              MultipleTripTab(),
+              GoAndBackTabs(),
+              GoTab(),
+
+
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/flight/presentation/widgets/multiple trips_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/flight/presentation/widgets/trip_item.dart';
+
+class MultipleTripsItem extends StatelessWidget {
+  const MultipleTripsItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        TripItems(text: 'التاريخ'),
+        TripItems(text: "الى"),
+        TripItems(text: "من"),
+
+      ],
+    );
+  }
+}
+
+
